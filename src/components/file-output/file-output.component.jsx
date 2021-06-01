f7002958669ebbb912deffe83eea9015b8220b41
Chaim <chaim1026@gmail.com>
2021-06-01 22:04:53 +0300

    error fix

--- src/components/file-output/file-output.component.jsx
+++ src/components/file-output/file-output.component.jsx
@@ -3,11 +3,16 @@ import { connect } from 'react-redux';
 import './file-output.styles.css';
 
 
-const FileOutput = ({ loadedFile }) => (
-    <div className='file-output'>
-        <p className='loaded-file'>{loadedFile}</p>
-    </div>
-);
+const FileOutput = ({ loadedFile }) => {
+    console.log(loadedFile);
+    return (
+        <div className='file-output'>
+            {loadedFile.map((row, index) => (
+                <p className='loaded-file' key={index}>{row}</p>
+            ))}
+        </div>
+    )
+};
 
 const mapStateToProps = (state) => ({
     loadedFile: state.loadedFile.loadedFile
--- src/redux/loaded-file/loaded-file.utils.js
+++ src/redux/loaded-file/loaded-file.utils.js
@@ -3,21 +3,32 @@ export const cleanReceivedData = (loadedFile) => {
     let cleanData = [];
     let badLetters = [];
     let comment = false;
+    let type = '';
     for (let letter of loadedFileArray) {
         if (comment === false){
-            if ((cleanData[cleanData.length - 1] === '/' && letter === '*') || (cleanData[cleanData.length - 1] === '/' && letter === '/')){
+            if (cleanData[cleanData.length - 1] === '/' && letter === '*') {
                 cleanData.splice(cleanData.length - 1, 1)
                 comment = true;
+                type = '/*'
+            } else if (cleanData[cleanData.length - 1] === '/' && letter === '/') {
+                cleanData.splice(cleanData.length - 1, 1)
+                comment = true;
+                type = '//'
             } else {
                 cleanData.push(letter)
             }
         } else {
-            if ((badLetters[badLetters.length - 1] === '*' && letter === '/') || (letter === '\n')) {
+            if (badLetters[badLetters.length - 1] === '*' && letter === '/' && type === '/*') {
                 comment = false;
-            }
-            badLetters.push(letter)
+            } else if (letter === '\n' && type === '//') {
+                comment = false;
+            } 
+            badLetters.push(letter);
         }
     } 
 
-    return cleanData.join('');
+    let finalString = cleanData.join('');
+    let result = finalString.split('\n');
+
+    return result;
 };
